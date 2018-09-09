34e1b6802b77a20ced0519405ee8c2897187349b
ozwind <chewi0@hotmail.com>
2018-09-09 12:54:12 -0700

    Add files via upload

--- hikes.js
+++ hikes.js
@@ -127,6 +127,15 @@ function mouseout(img) {
 
 var data = [
     {
+        date: '9/9/2018',
+        duration: '3:20',
+        miles: 8.9,
+        trail: 'Weir Santiago',
+        url: 'https://www.alltrails.com/explore/recording/weir-santiago--13',
+        conditions: 'Sunny 64&deg;-88&deg;',
+        photos: ['https://www.alltrails.com/api/alltrails/photos/21321572/image?size=extra_large&api_key=3p0t5s6b5g4g0e8k3c1j3w7y5c3m4t8i','https://www.alltrails.com/api/alltrails/photos/21321684/image?size=extra_large&api_key=3p0t5s6b5g4g0e8k3c1j3w7y5c3m4t8i']
+    },
+    {
         date: '9/1/2018',
         duration: '4:17',
         miles: 12.4,
