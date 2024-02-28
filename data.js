e12b317224ad922b34af85abe5092f1641f2f4ac
ozwind <chewi0@hotmail.com>
2024-02-28 11:49:14 -0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,14 @@
 var data = [
     {
+        date: '2/28/2024',
+        duration: '3:19',
+        miles: 8.8,
+        trail: 'Santiago Creek',
+        url: 'https://www.alltrails.com/explore/recording/morning-hike-3d387d2-154',
+        conditions: 'Sunny 48°/61°',
+        photos: ['https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNzExNjgzMTgvODcwOTk0OTA5ZmViODNhOTE0Mjg1MDU1YTNiMDliMzIuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==','https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNzExNjgzMjUvYTJmNzFmZDJlNTNlNmE5MmFkOTcyZTJkZDBkODVkYzIuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==']
+    },
+    {
         date: '2/13/2024',
         duration: '4:05',
         miles: 11.2,
