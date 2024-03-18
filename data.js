957390535129be62fc1e9e77024aacd8429b90e1
ozwind <chewi0@hotmail.com>
2024-03-18 12:53:48 -0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,14 @@
 var data = [
     {
+        date: '3/18/2024',
+        duration: '3:54',
+        miles: 10.8,
+        trail: 'Puma ridge',
+        url: 'https://www.alltrails.com/explore/recording/morning-hike-8efb100-149',
+        conditions: 'Sunny 49°/64°',
+        photos: ['https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNzE4NzQ1NDcvZTI5ODRmOGUzZGNlZDNlODFhZDhmYmE4ZjAwZTE2ZTAuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==','https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNzE4NzQ1NDIvYjkwMzQxOTU4NTVmNDQxNDlmOTg1YzlhZWI5ZGQ0YzguanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==']
+    },
+    {
         date: '3/11/2024',
         duration: '4:10',
         miles: 11.1,
